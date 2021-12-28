55398c6abb534be7b9cde7b22e71b9602bc759d7
dimmxx@gmail.com <dimmxx@gmail.com>
2021-12-28 07:00:10 -0800

    changelog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shop32';
+export class AppComponent implements AfterViewInit{
+
+  @ViewChild('appTitle')
+  appTitle!: ElementRef<HTMLHeadingElement>;
+
+  ngAfterViewInit(): void {
+    this.appTitle.nativeElement.textContent = 'Shop32 application';
+  }
 }
